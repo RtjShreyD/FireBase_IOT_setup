ecf61fbb03f07a41f4663ab4cc6fe8d0e69e1c8f
RtjShreyD <shreyanshece1041@gmail.com>
2019-10-14 14:30:17 +0530

    String Parse method

--- FirebaseDemo_ESP8266/FirebaseDemo_ESP8266.ino
+++ FirebaseDemo_ESP8266/FirebaseDemo_ESP8266.ino
@@ -29,111 +29,15 @@ void setup()
     
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 
-    Firebase.set("BOX_STATUS",0);  
+    Firebase.set("BOX_STATUS","!1234?$3456@");  
 }
 
-int n = 0;
+String nx = "";
 
 void loop() 
 {
     
-     // set value  
-  n=Firebase.getInt("BOX_STATUS");  
-  
-  // handle error  
-  if (n==1) 
-  {  
-      Serial.println("BOX is OPEN");  
-      //digitalWrite(D1,HIGH);  
-      Serial.println(Firebase.error());    
-      return;  
-      delay(100);  
-  }  
- else
- {  
-      Serial.println("BOX is CLOSE");  
-      //digitalWrite(D1,LOW);  
- }  
-    
-    
-    
-    
-    
-    /*// set value
-    Firebase.setFloat("number", 42.0);
-    
-    // handle error
-    if (Firebase.failed()) 
-    {
-        Serial.print("setting /number failed:");
-        Serial.println(Firebase.error());  
-        return;
-    }
-    
-    delay(1000);
-    // update value
-    Firebase.setFloat("number", 43.0);
-    
-    // handle error
-    if (Firebase.failed()) 
-    {
-        Serial.print("setting /number failed:");
-        Serial.println(Firebase.error());  
-        return;
-    }
-
-    delay(1000);
-
-    // get value 
-    Serial.print("number: ");
-    Serial.println(Firebase.getFloat("number"));
-    delay(1000);
-
-    // remove value
-    Firebase.remove("number");
-    delay(1000);
-    // set string value
-    Firebase.setString("message", "hello world");
-
-    // handle error
-    if (Firebase.failed()) 
-    {
-        Serial.print("setting /message failed:");
-        Serial.println(Firebase.error());  
-        return;
-    }
-    
-    delay(1000);
-    
-    // set bool value
-    Firebase.setBool("truth", false);
-    
-    // handle error
-    if (Firebase.failed()) 
-    {
-        Serial.print("setting /truth failed:");
-        Serial.println(Firebase.error());  
-        return;
-    }
-    
-    delay(1000);
-    
-    // append a new value to /logs
-    String name = Firebase.pushInt("logs", n++);
-    
-    // handle error
-    if (Firebase.failed()) 
-    {
-        Serial.print("pushing /logs failed:");
-        Serial.println(Firebase.error());  
-        return;
-    }
-    
-    Serial.print("pushed: /logs/");
-    Serial.println(name);
-    
-    delay(1000);*/
-
-    
-    
+     // GET value  
+  nx=Firebase.getString("BOX_STATUS");  
+  Serial.println(nx);    
 }
